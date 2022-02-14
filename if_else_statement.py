3d64e8c0e2e3fa428c4a23cc06e7cb2a164687ba
flapjackstan <elm.s.camargo@gmail.com>
2022-02-14 08:06:01 -0800

    moved if else example

--- if_else_statement.py
+++
@@ -1,8 +0,0 @@
-# the program is asking for input
-x = input("instagram username: ")
-
-# if-else statement to check if username is equal to flapjackstan
-if x == "flapjackstan":
-    print("hello elmer") # print is an example output
-else:
-    print("you are not elmer")
